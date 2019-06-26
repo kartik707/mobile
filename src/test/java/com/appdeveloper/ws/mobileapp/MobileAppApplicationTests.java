d45f6ea4221273af5fda08cc492cc985bc2203d4
kartik707 <luckydrive77@gmail.com>
2019-06-26 09:16:28 +0530

    save

---
+++ src/test/java/com/appdeveloper/ws/mobileapp/MobileAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appdeveloper.ws.mobileapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobileAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
